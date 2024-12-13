0fb7a5203977d87d0e7c8c6eaf00054c7416d855
Tiffany Zhang <tzhang@lcs.on.ca>
2024-12-13 14:45:54 -0500

    Initial Commit

---
+++ CustomShapeExercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CustomShapeExercise
+//
+//  Created by xinyu zhang on 2024-12-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CustomShapeExercise/CustomShapeExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomShapeExerciseApp.swift
+//  CustomShapeExercise
+//
+//  Created by xinyu zhang on 2024-12-13.
+//
+
+import SwiftUI
+
+@main
+struct CustomShapeExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
